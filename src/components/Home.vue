7892f7039a0a0de59b81521559e0bbb37c4ee43c
chen <270210539@qq.com>
2020-06-19 15:40:27 +0800

    111

---
+++ src/components/Home.vue
@@ -0,0 +1,26 @@
+<template>
+<div>
+    <el-button type="info" @click="logout">退出</el-button>
+</div>
+</template>
+
+<script>
+export default {
+  data () {
+    // 这里存放数据
+    return {
+
+    }
+  },
+  methods: {
+    logout () {
+      window.sessionStorage.clear()
+      this.$router.push('/login')
+    }
+  }
+}
+</script>
+<style lang='less' scoped>
+//@import url(); 引入公共css类
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,49 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Router from 'vue-router'
+// import Home from '../views/Home.vue'
+import Login from '../components/Login.vue'
+import Home from '../components/Home.vue'
 
-Vue.use(VueRouter)
+Vue.use(Router)
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+const router = new Router({
+  mode: 'hash',
+  routes: [
+    {
+      path: '/',
+      redirect: {
+        name: 'Login'
+      }
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '/home',
+      name: 'Home',
+      component: Home
+    }
+  ]
+})
 
-const router = new VueRouter({
-  routes
+// const router = new VueRouter({
+//   routes
+// })
+
+// 挂载路由导航守卫  如果用户没有登录，直接通过url访问特定页面，需要重新导航到登录页面
+router.beforeEach((to, from, next) => {
+  // to 将要访问的路径
+  // from 代表从那个路径跳转而来
+  // next 是一个函数，表示放行
+  // next() 放行   next('/login')  强制跳转
+
+  if (to.path === '/login') return next()
+  // 获取token
+  const tokenStr = window.sessionStorage.getItem('token')
+  if (!tokenStr) return next('/login')
+  next()
 })
 
 export default router
